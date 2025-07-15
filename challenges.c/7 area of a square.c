fd27b77c09fcc9ab99e0a27a9f9a793774655763
Prottoy-roy <prottoyroy4@gmail.com>
2025-07-15 19:29:39 +0600

    update challenges.c

---
+++ challenges.c/7 area of a square.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  int square,side;
+  printf("enter side: ");
+  scanf("%d",&side);
+  square = side * side;
+  printf("square of a area is: %d",square);
+  return 0;
+}
