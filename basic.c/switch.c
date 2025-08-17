940bb34afe133728b9796d04b5e66d810a142419
Prottoy-roy <prottoyroy4@gmail.com>
2025-08-17 13:59:57 +0600

    add switch

---
+++ basic.c/switch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  int day;
+  printf("enter day(1-7):");
+  scanf(" %d",&day);
+  switch(day) {
+    case 1:
+     printf("$-today is sunday-$\n");
+     break;
+    case 2:
+     printf("$-today is monday-$\n");
+     break;
+    case 3:
+     printf("$-today is tuesday-$\n");
+     break;
+    case 4:
+     printf("$-today is wednesday-$\n");
+     break;
+    case 5:
+     printf("$-today is thursday-$\n");
+     break;
+    case 6:
+     printf("$-today is friday-$\n");
+     break;
+    case 7:
+     printf("$-today is satarday-$\n");
+     break;
+    default:
+     printf("$-not a valid day-$\n");
+     break;
+  }
+  printf("*.......................*\n");
+  printf("this is a day calculater\n");
+  return 0;
+}
---
+++ basic.c/switch_if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+  int day;
+  printf("enter a day(1-7):");
+  scanf(" %d",&day);
+  if(day==1) {
+    printf("$-today is satarday-$\n");
+  }else if(day==2) {
+    printf("$-today is Sunday-$\n");
+  }else if(day==3) {
+    printf("$-today is monday-$\n");
+  }else if(day==4) {
+    printf("$-today is tuesday-$\n");
+  }else if(day==5) {
+    printf("$-today is wednesday-$\n");
+  }else if(day==6) {
+    printf("$-today is thursday-$\n");
+  }else if(day==7) {
+    printf("$-today is friday-$\n");
+  }
+  return 0;
+}
