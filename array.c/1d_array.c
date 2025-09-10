2c818c3ff8c896b10a144def1f1f848a447e95a7
Prottoy-roy <prottoyroy4@gmail.com>
2025-09-10 16:27:24 +0600

    add arrays

---
+++ array.c/1d_array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  printf("%d",arr[4]);//output = 4
+  return 0;
+}
---
+++ array.c/2d_array.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int array[2][3] = {{1,2,3},{1,2,3}};//array[2]=2rows[3]=3columns
+  printf("%d",array[0][2]);//out put = 3
+  return 0;
+}
