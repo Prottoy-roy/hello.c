8db58a8d5861d9b6a3d2fd632e0d21aa23995b14
Prottoy-roy <prottoyroy4@gmail.com>
2025-09-10 20:04:14 +0600

    add reverse array

--- array.c/reverse_array.c
+++ array.c/reverse_array.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 //write a function to reverse an array
-int reverse(int arr[]);
+int reverse(int arr[],int n);
+int numbers(int arr[],int n);
 int main(void)
 {
   int arr[]={1,2,3,4,5,6};
-  reverse(arr);
+  reverse(arr,6);
+  numbers(arr,6);
   return 0;
 }
-int reverse(int arr[]){
-  for(int i=5;i>=0;i--){
-    printf("%d\n",arr[i]);
+int reverse(int arr[],int n){
+  for(int i=0;i<n/2;i++){
+    int firstvalue=arr[i];
+    int secvalue=arr[n-i-1];
+    arr[i]=secvalue;
+    arr[n-i-1]=firstvalue;
+  }
+}
+int numbers(int arr[],int n){
+  for(int i=0;i<n;i++){
+    printf("%d \t",arr[i]);
   }
 }
