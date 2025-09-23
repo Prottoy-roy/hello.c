336715ecb4e18021a3a521b875a53bc4dab2b4fe
Prottoy-roy <prottoyroy4@gmail.com>
2025-09-23 21:48:20 +0600

    add string

---
+++ string.c/string12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+  char str[100];
+  char ch;
+  printf("Enter your name:");
+  int i=0;
+  while(ch != '\n'){
+    scanf("%c",&ch);
+    str[i]= ch;
+    i++;
+  }
+  str[i]='\0';
+  puts(str);
+  return 0;
+}
