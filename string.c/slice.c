4cdc9545df0ea3e303e65ed4fd87ccf95f629804
Prottoy-roy <prottoyroy4@gmail.com>
2025-09-29 12:07:00 +0600

    add slice

---
+++ string.c/slice.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void slice(char str[],int m,int n);
+int main(void)
+{
+  char str[]="helloword";
+  slice(str,3,6);
+  return 0;
+}
+void slice(char str[],int m,int n){
+  char newstr[100];
+  int j=0;
+  for(int i=m;i<=n;i++,j++){
+    newstr[j]=str[i];
+  }
+  newstr[j]='\0';
+  puts(newstr);
+}
