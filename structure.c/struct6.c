ae3d5129578dbf0c5b7d675e21a31317f8e68bc0
Prottoy-roy <prottoyroy4@gmail.com>
2025-10-08 14:10:38 +0600

    add stuct6

---
+++ structure.c/struct6.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+typedef struct student{
+  char name[100];
+  float cgpa;
+  int roll;
+}stu;
+void stuinfo(struct student s1);
+int main(void){
+  stu s1 = {"prottoy",7.50,1010};
+  stuinfo(s1);
+  return 0;
+}
+void stuinfo(struct student s1){
+  printf("Student info:\n");
+  printf("name is %s\n",s1.name);
+  printf("cgpa is %f\n",s1.cgpa);
+  printf("roll is %d\n",s1.roll);
+}
